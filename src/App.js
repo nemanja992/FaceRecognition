836efca186dbdc9d82a1ca51d338bd56f5065b41
Nemanja Muncan <nemanja_muncan@yahoo.com>
2018-05-18 19:12:27 +0200

    changed img src and text

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+onInputChange = (event) => {
+  console.log(event);
+}
+
   render() {
     return (
       <div className="App">
@@ -33,7 +43,7 @@ class App extends Component {
         <Navigation />
         <Logo /> 
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
       </div>
     );
   }
